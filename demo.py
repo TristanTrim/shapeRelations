a5472545b849e70386e7e37b0efbef31df99b96d
tristantrim <tristantrim@gmail.com>
2015-03-23 09:33:00 -0300

    The dumbness demo is completed!

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
 
 import pygame
 import random
-import math
+
+import shapes
 
 # Window dimensions
 width = 640
@@ -11,11 +12,64 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
-rad=25
-while running:
+bigCircle = shapes.Circle()
+bigCircleCopy = shapes.Circle()
+smallCircle = shapes.Circle(radius=25)
+square = shapes.Square(position=(250,250),radius=100)
+circle = shapes.Circle(position=(100,100),radius=40)
+oppositeCircle = shapes.Circle(radius=5)
+oppositeSquare = shapes.Square(radius=20)
+smallSquare = shapes.Square(position=(200,200),radius=10)
+otherSquare = shapes.Square(position=(400,100))
+otherSmallSquare = shapes.Square(radius=20)
+otherOtherSmallSquare = shapes.Square(radius=20)
 
-    for ii in range(360):
-        screen.set_at([int(100+math.cos(ii)*rad),int(100+math.sin(ii)*rad)],(222,0,0))
+foo=0
+bar=180
+while running:
+    screen.fill((0,0,0))
+    foo+=1
+    if foo>360:
+        foo=0
+    bar+=1
+    if bar>360:
+        bar=0
+
+    #bigCircle.xPosition,bigCircle.yPosition=square.drawShape(foo)
+    bigCircle.xPosition,bigCircle.yPosition=square.drawSub(bigCircle,foo)
+    bigCircleCopy.xPosition,bigCircleCopy.yPosition=square.drawAdd(bigCircle,foo)
+    oppositeCircle.xPosition,oppositeCircle.yPosition=square.drawShape(bar)
+    oppositeSquare.xPosition,oppositeSquare.yPosition=square.drawShape(bar)
+    otherSquare.xPosition,otherSquare.yPosition=bigCircleCopy.drawRotAdd(otherSquare,foo,foo)
+    smallSquare.xPosition,smallSquare.yPosition=otherSquare.drawRotated(foo,foo)
+    otherSmallSquare.xPosition,otherSmallSquare.yPosition=circle.drawSub(otherSmallSquare,foo)
+    otherOtherSmallSquare.xPosition,otherOtherSmallSquare.yPosition=circle.drawAdd(otherSmallSquare,foo)
+
+    for ii in range(0,720,1):
+        ii=float(ii)/2
+        screen.set_at(bigCircle.drawShape(ii),(222,0,0))
+        screen.set_at(bigCircleCopy.drawShape(ii),(222,222,222))
+       #screen.set_at(bigCircle.drawAdd(smallCircle,ii),(222,222,0))
+       #screen.set_at(bigCircle.drawSub(smallCircle,ii),(222,222,0))
+        screen.set_at(square.drawShape(ii),(0,222,0))
+        screen.set_at(square.drawAdd(bigCircle,ii),(222,222,0))
+        screen.set_at(square.drawSub(bigCircle,ii),(222,222,0))
+       #screen.set_at(smallCircle.drawShape(ii),(0,0,222))
+
+        screen.set_at(circle.drawShape(ii),(0,0,222))
+        screen.set_at(circle.drawAdd(otherSmallSquare,ii),(0,0,222))
+        screen.set_at(circle.drawSub(otherSmallSquare,ii),(0,0,222))
+        screen.set_at(oppositeCircle.drawShape(ii),(0,222,0))
+        screen.set_at(oppositeSquare.drawShape(ii),(55,222,55))
+
+        screen.set_at(otherSmallSquare.drawShape(ii),(222,0,222))
+        screen.set_at(otherOtherSmallSquare.drawShape(ii),(222,0,222))
+
+        screen.set_at(smallSquare.drawRotated(ii,foo),(222,0,222))
+
+        screen.set_at(otherSquare.drawRotated(ii,foo),(0,222,222))
+        screen.set_at(otherSquare.drawRotAdd(smallSquare,ii,foo),(0,222,222))
+        screen.set_at(otherSquare.drawRotSub(smallSquare,ii,foo),(0,222,222))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
---
+++ shapes.py
@@ -0,0 +1,94 @@
+
+import math
+
+class Shape():
+    def drawShape(self,degree):
+        x,y=self.shape(degree)
+        return(int(self.xPosition+x),int(self.yPosition+y))
+
+    def add(self,other,degree):
+       otherx,othery=other.shape(degree)
+       x,y=self.shape(degree)
+       return(otherx+x,othery+y)
+    def drawAdd(self,other,degree):
+        x,y=self.add(other,degree)
+        return(int(self.xPosition+x),int(self.yPosition+y))
+
+    def sub(self,other,degree):
+       otherx,othery=other.shape(degree)
+       x,y=self.shape(degree)
+       return(x-otherx,y-othery)
+    def drawSub(self,other,degree):
+        x,y=self.sub(other,degree)
+        return(int(self.xPosition+x),int(self.yPosition+y))
+
+    def rotated(self,degree,shift):
+        newdegree=degree+shift
+        if newdegree>360:
+            newdegree-=360
+        x,y=self.shape(newdegree)
+        vect=math.sqrt(x**2+y**2)
+        x=math.cos(math.radians(degree))*vect
+        y=math.sin(math.radians(degree))*vect
+        return(x,y)
+    def drawRotated(self,degree,shift):
+        x,y=self.rotated(degree,shift)
+        return(int(self.xPosition+x),int(self.yPosition+y))
+
+    def drawRotAdd(self,other,degree,shift):
+        newdegree=degree+shift
+        if newdegree>360:
+            newdegree-=360
+        x,y=self.add(other,newdegree)
+        vect=math.sqrt(x**2+y**2)
+        x=math.cos(math.radians(degree))*vect
+        y=math.sin(math.radians(degree))*vect
+        return(int(self.xPosition+x),int(self.yPosition+y))
+
+    def drawRotSub(self,other,degree,shift):
+        newdegree=degree+shift
+        if newdegree>360:
+            newdegree-=360
+        x,y=self.sub(other,newdegree)
+        vect=math.sqrt(x**2+y**2)
+        x=math.cos(math.radians(degree))*vect
+        y=math.sin(math.radians(degree))*vect
+        return(int(self.xPosition+x),int(self.yPosition+y))
+
+
+class Circle(Shape):
+    def __init__(self,position=(50,50),radius=25):
+        self.size=radius
+        self.xPosition=position[0]
+        self.yPosition=position[1]
+
+    def shape(self,degree):
+        x=math.cos(math.radians(degree))*self.size
+        y=math.sin(math.radians(degree))*self.size
+        return(x,y)
+
+class Square(Shape):
+    def __init__(self,position=(50,50),radius=25):
+        self.size=radius
+        self.xPosition=position[0]
+        self.yPosition=position[1]
+    def shape(self,degree):
+        if degree >= 315:
+            x=self.size
+            y=self.size*(degree-360)/45
+        elif degree < 45:
+            x=self.size
+            y=self.size*degree/45
+        elif degree < 135:
+            x=self.size*(90-degree)/45
+            y=self.size
+        elif degree < 225:
+            x=-self.size
+            y=self.size*(180-degree)/45
+        elif degree < 315:
+            x=self.size*(degree-270)/45
+            y=-self.size
+        return(x,y)
+
+
+
